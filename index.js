83a876a38f18e62f406c2b11b6e6aa6dc8544abd
Kamil Kałuża <kamil321.94@gmail.com>
2022-12-11 07:37:53 +0100

    work only once?

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const el = document.getElementById("d");
+const hamburger = document.querySelector(".hamburger")
 
-
-el.addEventListener('click', () => {
+hamburger.addEventListener('click', () => {
     const item = document.querySelector(".nav-items");
     item.className = item.className = "slider";
 
 
-    const hamburger = document.querySelector("#hamburger");
+    const hamburger = document.querySelector(".hamburger");
     hamburger.remove();
 
-    const slider = document.querySelector(".slider");
+    const d = document.querySelector(".close-container");
     const closeBtn = document.createElement("img");
     closeBtn.src ="./assets/images/icon-menu-close.svg";
     closeBtn.className= "close-btn";
 
-    slider.appendChild(closeBtn)
+    d.appendChild(closeBtn)
+
+});
+
+const closeBtn = document.querySelector(".close-container");
+
+closeBtn.addEventListener('click', () => {
+    
+    const item = document.querySelector(".slider");
+    item.className = item.className = "nav-items";
+
+    const closeBtn = document.querySelector(".close-container");
+    closeBtn.removeChild(closeBtn.firstChild);
+
+    hamburgerContainer = document.querySelector(".hamburger-container")
+
+    const hamburger = document.createElement("img");
+    hamburger.src ="./assets/images/icon-menu.svg";
+    hamburger.className= "hamburger";
+    hamburgerContainer.appendChild(hamburger)
 
 })
-
-// addEventListener('click', () => {
-//     const closeBtn = document.querySelector(".close-btn");
-//     const item = document.querySelector(".slider");
-
-// })
